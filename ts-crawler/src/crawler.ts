00163c9b98f386bda51c1bb4a7fcdf72709c78e9
llx123 <573381010@qq.com>
2020-12-14 00:34:56 +0800

    cheerio

--- ts-crawler/src/crawler.ts
+++ ts-crawler/src/crawler.ts
@@ -1,4 +1,9 @@
 import superagent from 'superagent';
+import cheerio from 'cheerio';
+
+interface Info {
+  title: string
+}
 
 class Crawler {
   private secret = 'secretKey';
@@ -7,10 +12,21 @@ class Crawler {
   constructor() {
     this.getHtml()
   }
+  getInfo(result: string) {
+    const $ = cheerio.load(result);
+    const items = $('.course-item');
+    const infoList: Info[] = [];
+    items.map((index, element) => {
+      const descs = $(element).find('.course-desc');
+      const title = descs.eq(0).text();
+      infoList.push({
+        title
+      })
+    })
+  }
   async getHtml() {
     const result = await superagent.get(this.url);
-    console.log(result.text);
-
+    this.getInfo(result.text);
   }
 }
 
